dc60c30fb46a8d5b273b6d383b82868830330acb
Alibi <46373160+Alibii@users.noreply.github.com>
2025-01-10 16:12:37 +0500

    Add files via upload

---
+++ JS_HW_1/script1.js
@@ -0,0 +1,5 @@
+let name = "Alibi"
+let age = 25
+let isStudent = true
+
+console.log(` ${name}, ${age}, ${isStudent}`);
---
+++ JS_HW_1/script4_1.js
@@ -0,0 +1,11 @@
+const book = {
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967
+  };
+  
+  console.log("Книга:", book);
+  console.log("Название:", book.title);
+  console.log("Автор:", book.author);
+  console.log("Год издания:", book.year);
+  
