4d26ea8f846c6162902b939501bc767e562c35e5
Alibi <46373160+Alibii@users.noreply.github.com>
2025-01-16 10:25:41 +0500

    Update script5.js

---
+++ JS_HW_1/script5.js
@@ -0,0 +1,6 @@
+const personName = "Алексей";
+const personAge = 30;
+const personCity = "Москва";
+
+const personInfo = `Имя: ${personName}, Возраст: ${personAge}, Город: ${personCity}`;
+console.log(personInfo);
