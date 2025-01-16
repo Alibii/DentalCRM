4875f0ebbc3ecb1cece0f97d1785b1e193cefeb3
Alibi <46373160+Alibii@users.noreply.github.com>
2025-01-16 10:25:01 +0500

    Update script3.js

---
+++ JS_HW_1/script3.js
@@ -0,0 +1,11 @@
+let stringValue = "Пример строки";
+let numberValue = 42;
+let booleanValue = true;
+let nullValue = null;
+let undefinedValue;
+
+console.log("Тип stringValue:", typeof stringValue);
+console.log("Тип numberValue:", typeof numberValue);
+console.log("Тип booleanValue:", typeof booleanValue);
+console.log("Тип nullValue:", typeof nullValue); // Это объект (особенность JS)
+console.log("Тип undefinedValue:", typeof undefinedValue);
